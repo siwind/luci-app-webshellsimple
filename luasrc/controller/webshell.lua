cb51fba2bb3ba40ad54519541eb0d247a0324434
siwind <yinqingwang@163.com>
2020-02-13 19:29:23 +0800

    modify ui

--- luasrc/controller/webshell.lua
+++ luasrc/controller/webshell.lua
@@ -8,56 +8,42 @@ function index()
 	entry({"admin", "services", "cmd_run"}, call("cmd_run"), nil)
 end
 
-function string.starts(String,Start)
-	return string.sub(String,1,string.len(Start))==Start
-end
-
 function cmd_run()
-	local re =""
-	local path = luci.dispatcher.context.requestpath
-	local rv   = { }
 	local cmd = luci.http.formvalue("cmd")
 	local path = luci.http.formvalue("path")
-	local runcmd="cd "..path.."&&"..cmd
 
+	local re = ""
+	if cmd ~= "" then re = luci.sys.exec("cd "..path.." && "..cmd .. " 2>&1") end
 
-	local shellpipe = io.popen(runcmd.." 2>&1","rw")
-	re = shellpipe:read("*a")
-	shellpipe:close()
-	if not re then
-		re=""
+	local pwdcmd = "cd "..path.." && pwd "
+	if cmd == "cd" or string.sub(cmd,1,3) == "cd " then
+		pwdcmd = "cd "..path.." && " ..cmd.." && pwd "
 	end
 
-	local pwdcmd = "cd "..path.."&& pwd 2>&1"
-	if string.starts(cmd,"cd") then
-		pwdcmd = "cd "..path.."&&"..cmd.."&& pwd 2>&1"
-	end
-	local shellpipe = io.popen(pwdcmd,"rw")
-	local newpath = shellpipe:read("*a")
-	shellpipe:close()
+	local newpath = luci.sys.exec(pwdcmd)
+	if newpath == "" then newpath = path end
 
-	if not newpath then
-		newpath=path
-	end
+	local ls = luci.sys.exec("ls " .. newpath)
+	local host = luci.sys.exec("uname -n")
+	local user = luci.sys.user.getuser(nixio.getuid())['name']
 
-	local pathcmd = io.popen("ls "..newpath.."2>&1","rw")
-	local ls = pathcmd:read("*a")
-	pathcmd:close()
+	local rv = { }; rv['res'] = re; rv['path'] = newpath; rv['ls'] = ls; rv['host'] = host; rv['user'] = user
 
-	ls = string.gsub(ls, "\n", ",")
-	re = string.gsub(re, "\n", "\r\n")
+	luci.http.prepare_content("application/json")
+	luci.http.write_json(rv)
+end
 
-	rv[#rv+1]=re
-	rv[#rv+1]=newpath
-	rv[#rv+1]=ls
+function cmd_upload()
+    local filecontent = luci.http.formvalue("upload-file")
+    local filename = luci.http.formvalue("upload-filename")
+    local uploaddir = luci.http.formvalue("upload-dir")
+    local filepath = uploaddir..filename
+    local url = luci.dispatcher.build_url('admin', 'system', 'filebrowser')
 
+    local fp
+    fp = io.open(filepath, "w")
+    fp:write(filecontent)
+    fp:close()
+    luci.http.redirect(url..'?path='..uploaddir)
 
-
-	if #rv > 0 then
-		luci.http.prepare_content("application/json")
-		luci.http.write_json(rv)
-		return
-	end
-
-	luci.http.status(404, "No such device")
 end
